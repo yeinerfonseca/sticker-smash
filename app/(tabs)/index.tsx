91602ece94a7127c01f46f18f8558656c4ecb7f1
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-11-25 16:12:44 -0500

    feat: create project and add home and about pages in tab

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,28 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      <Link href="/about" style={styles.button}>Go to About screen</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
