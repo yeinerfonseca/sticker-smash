841556e4f3f0f2b5bfdaa6fd6e2e0d139ce5f28c
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-11-25 16:49:58 -0500

    feat: display selected image instead of placeholder in image-viewer

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,41 @@
 import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import React from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = React.useState<string | undefined>(
+    undefined
+  );
+
+  async function pickImageAsync() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets.at(0)?.uri)
+    } else {
+      alert("You did not select any image");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
